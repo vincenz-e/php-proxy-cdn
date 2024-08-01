817fa57e5f608ba55535807833fed27cb75cb79e
TheSupremeGod <lorenzjacob.noel@yahoo.com>
2024-08-01 16:14:46 +0200

    Add error message and better error handling
    Added 400 response if src parameter is not set
    Fixed potential warning being thrown for empty array key
    Removed '?>' to avoid accidental printing of newline

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
+if(!isset($_GET['src'])) {
+  http_response_code(400)
+  exit('Invalid request, please include src link as GET parameter');
+}
+
 $file_path = $_GET['src'];
-$content_type = $_GET['ct'] or '';
+$content_type = $_GET['ct'] ?? '';
 
 
 if (substr($file_path, -4) === '.css') {
@@ -16,4 +21,3 @@ if ($content_type) {
 
 echo file_get_contents($file_path); // Load the content of file and print it to browser
 
-?>
